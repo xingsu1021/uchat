85dd090894fcb852f264e33a4d7abe106a736759
sftang <xingsu1021@sina.com>
2013-03-08 15:44:55 +0800

    拟定后端为redis存储

--- uchat/apps/chat/views.py
+++ uchat/apps/chat/views.py
@@ -1,5 +1,7 @@
 #coding=utf-8
 from uliweb import expose, functions
+import redis
+from etc.config import UConfig
 
 @expose('/chat')
 ########################################################################
@@ -9,6 +11,12 @@ class Chat(object):
     #----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""
+        pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+        rc = redis.Redis(connection_pool=pool)
     
     def index(self):
+
+        #获取session
+        if not request.session.get('user'):
+            return redirect(url_for('main.views.index')) 
         return {}
---
+++ uchat/apps/etc/config.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*- #
+
+UConfig = {}
+
+#CM server
+UConfig['REDIS_HOST']='localhost'
+UConfig['REDIS_PORT']=6379
+UConfig['REDIS_DB'] = 0
+UConfig['REDIS_PASSWORD'] = ''
+
+#版本号
+UConfig['VER']='1.0.0'
